4bf0dadf1970e51d8104052e37c7a50c00e0e1b5
Ben Ostrovsky <ben.leo.ost@gmail.com>
2022-09-04 23:04:32 -0700

    Paragraph styling

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from bs4 import BeautifulSoup
 with open('sec1.html', 'r') as html_file:
     content = html_file.read()
+    real_pars=[]
+    soup = BeautifulSoup(content, 'lxml')
+    pars_html=soup.find_all('p')
     
-    print(content)
+    for par in pars_html:
+        if len(par)>=340:
+            real_pars.append(par)
+    print(real_pars)
