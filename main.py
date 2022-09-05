c6b180d10dd5b1a5185c55720b77292d77e0c8e7
Ben Ostrovsky <ben.leo.ost@gmail.com>
2022-09-05 00:53:11 -0700

    Collect Accurate Paragraph and Remove Page Count

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from bs4 import BeautifulSoup
+import re
+
+# Opening the HTML File. 
 with open('sec1.html', 'r') as html_file:
     content = html_file.read()
-    real_pars=[]
-    soup = BeautifulSoup(content, 'lxml')
-    pars_html=soup.find_all('p')
     
+#Lists - Empty
+    pargs=[]
+
+#Parsing through the HTML to find instances of desired class - paragraph in this case.
+    soup = BeautifulSoup(content, 'html.parser')
+    pars_html=soup.find_all('p', {'class': re.compile('TX')})
+    i=1
+#Collecting instances, un-textified
     for par in pars_html:
+        pargs.append(par.text)
+#Finding Suitable Instance
+    for par in pargs:
         if len(par)>=340:
-            real_pars.append(par)
-    print(real_pars)
+#Removing Page Count
+            par=str(par)
+            if 'page' in par:
+                x=par.index('page')
+                par=par.replace((par[x:x+8]), "")
+#Printing Instances
+            print(f'PARAGRAPH {i}:  {par}\n\n')
+            i+=1
+#Removing access data
+        else:
+            pargs.remove(par)
+    
