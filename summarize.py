89b686317422aa186299e18b2d3b1ac5398b0ecd
Ben Ostrovsky <ben.leo.ost@gmail.com>
2022-09-17 23:07:45 -0700

    Further Summary Progress

--- summarize.py
+++ summarize.py
@@ -1,4 +1,23 @@
+#Imports for summarizing it using nltk
 import scrape
+# import nltk
+# from nltk.tokenize import RegexpTokenizer, sent_tokenize
+# from nltk.corpus import stopwords
+# from nltk.stem.snowball import SnowballStemmer
+# import operator
+# import matplotlib.pyplot as plt
 
+#Assingning variables based on imports
+# stopwords = stopwords.words('english')
+# tokenizer = RegexpTokenizer(r'\w+')
+# stemmer = SnowballStemmer('english')
+raw=scrape.fin
+
+for value in raw:
+    if value.startswith('SUBSECTION')==True:  
+        if inSubsec==True:
+            None
+        else:
+            inSubsec=True
 
 
